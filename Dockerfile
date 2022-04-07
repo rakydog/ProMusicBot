7fcacf8a317776d673040e2c4897e5180c2617f8
RAKY DOG <92443575+rakydog@users.noreply.github.com>
2022-04-07 22:09:24 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
 FROM node:12.19-slim
 
-ENV USER=Raky Music
+ENV USER=evobot
 
+# install python and make
 RUN apt-get update && \
 	apt-get install -y python3 build-essential && \
 	apt-get purge -y --auto-remove
-
+	
+# create evobot user
 RUN groupadd -r ${USER} && \
-	useradd --create-home --home /home/Raky Music -r -g ${USER} ${USER}
-
+	useradd --create-home --home /home/evobot -r -g ${USER} ${USER}
+	
+# set up volume and user
 USER ${USER}
-WORKDIR /home/Raky Music
+WORKDIR /home/evobot
 
 COPY package*.json ./
 RUN npm install 
-VOLUME [ "/home/Raky Music" ]
+VOLUME [ "/home/evobot" ]
 
 COPY . .
 
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+/**
+ * Module Imports
+ */
 const { Client, Collection } = require("discord.js");
 const { readdirSync } = require("fs");
 const { join } = require("path");
 const { PREFIX } = require("./config.json");
 const client = new Client({ disableMentions: "everyone" });
+const status = "mobile"
 
+// 24/7 online
 const express = require('express');
-const app = express();
-const port = 3000 || 8000 || 5500;
 
-app.get('/', (req ,res) => {
-  res.send("Bot is online")
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at https://localhost:${port}`)
-})
-
+app.listen(3000, () => {
+  console.log('server started');
+});
 
 client.login(process.env.TOKEN);
 client.commands = new Collection();
@@ -23,21 +27,27 @@ client.prefix = PREFIX;
 client.queue = new Map();
 const cooldowns = new Collection();
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-const { BOT_PREFIX, BOT_STATUS2, BOT_STATUS3, BOT_PRESENCE } = require('./config.json');
+const {BOT_PREFIX, BOT_STATUS2, BOT_STATUS3 , BOT_PRESENCE } = require('./config.json');
 
+/**
+ * Client Events
+ */
 client.on("ready", () => {
-  function randomStatus() {
-    let status = [`${BOT_STATUS2}`, `${BOT_STATUS3}`, `${BOT_PREFIX}play ON ${client.guilds.cache.size} Servers`]
-    let rstatus = Math.floor(Math.random() * status.length);
+   function randomStatus() {
+ let status = [`${BOT_STATUS2}`,`${BOT_STATUS3}`, `${BOT_PREFIX}play ON ${client.guilds.cache.size} Servers`]
+let rstatus = Math.floor(Math.random() * status.length);
 
-    client.user.setActivity(status[rstatus], { type: `${BOT_PRESENCE}` });
-  }; setInterval(randomStatus, 30000)
+client.user.setActivity(status[rstatus], {type: `${BOT_PRESENCE}` });
+}; setInterval(randomStatus, 30000)
 
-  console.log('Bot is ready to play songs !')
+console.log('Bot is ready to play songs !')
 })
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
 
+/**
+ * Import all commands
+ */
 const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
   const command = require(join(__dirname, "commands", `${file}`));
@@ -75,7 +85,9 @@ client.on("message", async (message) => {
 
     if (now < expirationTime) {
       const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(`espere por favor ${timeLeft.toFixed(1)} Más segundos antes de reutilizar el \`${command.name}\` comando.`);
+      return message.reply(
+        `espere por favor ${timeLeft.toFixed(1)} Más segundos antes de reutilizar el \`${command.name}\` comando.`
+      );
     }
   }
 
